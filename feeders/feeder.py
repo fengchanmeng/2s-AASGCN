31d0f888fb0efebdb41acc17a65d96552f13b72b
fengchanmeng <91186668+fengchanmeng@users.noreply.github.com>
2021-09-22 17:15:00 +0800

    Add files via upload

---
+++ feeders/feeder.py
@@ -0,0 +1,204 @@
+import numpy as np
+import pickle
+import torch
+from torch.utils.data import Dataset
+import sys
+
+sys.path.extend(['../'])
+from feeders import tools
+
+#定义数据集
+class Feeder(Dataset):
+    #定义构造函数
+    def __init__(self, data_path, label_path,
+                 random_choose=False, random_shift=False, random_move=False,
+                 window_size=-1, normalization=False, debug=False, use_mmap=True):
+        """
+        
+        :param data_path: 
+        :param label_path: 
+        :param random_choose: If true, randomly choose a portion of the input sequence若为True，随机选择输入序列的一部分
+        :param random_shift: If true, randomly pad zeros at the begining or end of sequence若为Trye，则在序列前或结尾填充0
+        :param random_move: 
+        :param window_size: The length of the output sequence输出序列的长度
+        :param normalization: If true, normalize input sequence若为True，对输入序列进行归一化处理
+        :param debug: If true, only use the first 100 samples若为True，只使用前100个样本
+        :param use_mmap: If true, use mmap mode to load data, which can save the running memory
+        如果为true，则使用mmap模式加载数据，这样可以节省运行内存
+        mmap--地址的映射，一种内存映射文件的方法。将一个文件或者其他对象映射到进程的地址空间，实现文件磁盘地址和进程虚拟地址空间中。
+        mmap调用使得进程之间通过映射同一个普通文件实现共享内存。普通文件被映射到进程地址空间后，进程可以向访问普通内存一样对文件进行访问，
+        不必再调用read(),write()函数。
+        """
+
+        self.debug = debug
+        self.data_path = data_path
+        self.label_path = label_path
+        self.random_choose = random_choose
+        self.random_shift = random_shift
+        self.random_move = random_move
+        self.window_size = window_size
+        self.normalization = normalization
+        self.use_mmap = use_mmap
+        self.load_data()
+        if normalization:
+            self.get_mean_map()
+
+    def load_data(self):#加载数据
+        # data: N C V T M
+
+        try:
+            with open(self.label_path) as f:
+                self.sample_name, self.label = pickle.load(f)#pickle.load()函数将文件里的内容重写回python对象
+        except:
+            # for pickle file from python2
+            with open(self.label_path, 'rb') as f:
+                self.sample_name, self.label = pickle.load(f, encoding='latin1')
+
+        # load data
+        if self.use_mmap:
+            self.data = np.load(self.data_path, mmap_mode='r')
+        else:
+            self.data = np.load(self.data_path)
+        if self.debug:#if debug=True,使用前100个样本
+            self.label = self.label[0:100]#使用前100个样本标签
+            self.data = self.data[0:100]
+            self.sample_name = self.sample_name[0:100]
+
+    def get_mean_map(self):
+        data = self.data
+        N, C, T, V, M = data.shape
+        self.mean_map = data.mean(axis=2, keepdims=True).mean(axis=4, keepdims=True).mean(axis=0)
+        self.std_map = data.transpose((0, 2, 4, 1, 3)).reshape((N * T * M, C * V)).std(axis=0).reshape((C, 1, V, 1))
+
+    def __len__(self):
+        return len(self.label)
+
+    def __iter__(self):
+        return self
+
+    def __getitem__(self, index):
+        data_numpy = self.data[index]
+        label = self.label[index]
+        data_numpy = np.array(data_numpy)
+
+        if self.normalization:
+            data_numpy = (data_numpy - self.mean_map) / self.std_map
+        if self.random_shift:
+            data_numpy = tools.random_shift(data_numpy)
+        if self.random_choose:
+            data_numpy = tools.random_choose(data_numpy, self.window_size)
+        elif self.window_size > 0:
+            data_numpy = tools.auto_pading(data_numpy, self.window_size)
+        if self.random_move:
+            data_numpy = tools.random_move(data_numpy)
+
+        return data_numpy, label, index
+
+    def top_k(self, score, top_k):
+        rank = score.argsort()
+        hit_top_k = [l in rank[i, -top_k:] for i, l in enumerate(self.label)]
+        return sum(hit_top_k) * 1.0 / len(hit_top_k)
+
+
+def import_class(name):
+    components = name.split('.')
+    mod = __import__(components[0])
+    for comp in components[1:]:
+        mod = getattr(mod, comp)
+    return mod
+
+
+def test(data_path, label_path, vid=None, graph=None, is_3d=False):
+    '''
+    vis the samples using matplotlib
+    :param data_path: 
+    :param label_path: 
+    :param vid: the id of sample
+    :param graph: 
+    :param is_3d: when vis NTU, set it True
+    :return: 
+    '''
+    import matplotlib.pyplot as plt
+    loader = torch.utils.data.DataLoader( #数据集加载
+        dataset=Feeder(data_path, label_path),
+        batch_size=64,
+        shuffle=False,
+        num_workers=2)
+
+    if vid is not None:
+        sample_name = loader.dataset.sample_name
+        sample_id = [name.split('.')[0] for name in sample_name]
+        index = sample_id.index(vid)
+        data, label, index = loader.dataset[index]
+        data = data.reshape((1,) + data.shape)
+
+        # for batch_idx, (data, label) in enumerate(loader):
+        N, C, T, V, M = data.shape
+
+        plt.ion()
+        fig = plt.figure()
+        if is_3d:
+            from mpl_toolkits.mplot3d import Axes3D
+            ax = fig.add_subplot(111, projection='3d')
+        else:
+            ax = fig.add_subplot(111)
+
+        if graph is None:
+            p_type = ['b.', 'g.', 'r.', 'c.', 'm.', 'y.', 'k.', 'k.', 'k.', 'k.']
+            pose = [
+                ax.plot(np.zeros(V), np.zeros(V), p_type[m])[0] for m in range(M)
+            ]
+            ax.axis([-1, 1, -1, 1])
+            for t in range(T):
+                for m in range(M):
+                    pose[m].set_xdata(data[0, 0, t, :, m])
+                    pose[m].set_ydata(data[0, 1, t, :, m])
+                fig.canvas.draw()
+                plt.pause(0.001)
+        else:
+            p_type = ['b-', 'g-', 'r-', 'c-', 'm-', 'y-', 'k-', 'k-', 'k-', 'k-']
+            import sys
+            from os import path
+            sys.path.append(
+                path.dirname(path.dirname(path.dirname(path.abspath(__file__)))))
+            G = import_class(graph)()
+            edge = G.inward
+            pose = []
+            for m in range(M):
+                a = []
+                for i in range(len(edge)):
+                    if is_3d:
+                        a.append(ax.plot(np.zeros(3), np.zeros(3), p_type[m])[0])
+                    else:
+                        a.append(ax.plot(np.zeros(2), np.zeros(2), p_type[m])[0])
+                pose.append(a)
+            ax.axis([-1, 1, -1, 1])
+            if is_3d:
+                ax.set_zlim3d(-1, 1)
+            for t in range(T):
+                for m in range(M):
+                    for i, (v1, v2) in enumerate(edge):
+                        x1 = data[0, :2, t, v1, m]
+                        x2 = data[0, :2, t, v2, m]
+                        if (x1.sum() != 0 and x2.sum() != 0) or v1 == 1 or v2 == 1:
+                            pose[m][i].set_xdata(data[0, 0, t, [v1, v2], m])
+                            pose[m][i].set_ydata(data[0, 1, t, [v1, v2], m])
+                            if is_3d:
+                                pose[m][i].set_3d_properties(data[0, 2, t, [v1, v2], m])
+                fig.canvas.draw()
+                # plt.savefig('/home/lshi/Desktop/skeleton_sequence/' + str(t) + '.jpg')
+                plt.pause(0.01)
+
+
+if __name__ == '__main__':
+    import os
+
+    os.environ['DISPLAY'] = 'localhost:10.0'
+    data_path = "../data/ntu/xview/val_data_joint.npy"
+    label_path = "../data/ntu/xview/val_label.pkl"
+    graph = 'graph.ntu_rgb_d.Graph'
+    test(data_path, label_path, vid='S004C001P003R001A032', graph=graph, is_3d=True)
+    # data_path = "../data/kinetics/val_data.npy"
+    # label_path = "../data/kinetics/val_label.pkl"
+    # graph = 'graph.Kinetics'
+    # test(data_path, label_path, vid='UOD7oll3Kqo', graph=graph)
